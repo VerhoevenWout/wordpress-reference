e35cc405202265a6fefc36e20c92ea4feda426b2
Verhoevenwout <Wout@Wout-Verhoeven.local>
2017-06-26 11:42:09 +0100

    Update code

--- accordion-reference.php
+++ accordion-reference.php
@@ -1,67 +1,35 @@
 <!-- Make sure to use the lates version of ACF-pro (v5.+) -->
+<style>
+	#accordion{
+		margin-top: 50px;
+		margin-bottom: 50px;
+	}
+	.ui-accordion-header:hover{
+		cursor: pointer;
+	}
+	.ui-state-focus { outline: none; }
+	.ui-icon.iconOpen{
+		position: absolute;
+		left: -15px;
+		margin-top: -1px;
+	}
+	.ui-icon.iconOpen:before{
+		content: '-';
+	}
+	.ui-icon.iconClosed{
+		position: absolute;
+		left: -15px;
+		margin-top: -1px;
+	}
+	.ui-icon.iconClosed:before{
+		content: '+';
+	}
+</style>
 
 <?php if (is_page('8968')): ?>
 	<p><?php the_field('pre_accordion_content') ?></p>
 
 	<?php if( have_rows('accordion_repeater') ):?>
-    <style>
-      #accordion{
-        margin-top: 50px;
-        margin-bottom: 50px;
-      }
-      .ui-accordion-header:hover{
-        cursor: pointer;
-      }
-      .ui-state-focus { outline: none; }
-      .ui-icon.iconOpen{
-        position: absolute;
-        left: -15px;
-        margin-top: -1px;
-      }
-      .ui-icon.iconOpen:before{
-        content: '-';
-      }
-      .ui-icon.iconClosed{
-        position: absolute;
-        left: -15px;
-        margin-top: -1px;
-      }
-      .ui-icon.iconClosed:before{
-        content: '+';
-      }
-    </style>
-		<script>
-			$(function(){
-        // open and go to correct accordion-header when adding /#link-1 in url
-				var hashLink = window.location.hash;
-				if (hashLink){
-					// var hashLinkId = hashLink.split('-')[1];
-					setTimeout(function(){
-						$(hashLink).click();
-					}, 500);
-				}
-				$counter = 1;
-				$("#accordion h3").each(function(){
-					$(this).attr("id", "link-" + $counter);
-					$counter = $counter +1;
-				});
-				var icons = {
-					header: "iconClosed",    // custom icon class
-					activeHeader: "iconOpen" // custom icon class
-				};
-				$("#accordion").accordion({
-					icons: icons,
-					heightStyle: "content",
-					collapsible: true,
-					active: false,
-					activate: function( event, ui ){
-						if(!$.isEmptyObject(ui.newHeader.offset())) {
-						  $('html:not(:animated), body:not(:animated)').animate({ scrollTop: ui.newHeader.offset().top - 30 }, 'slow');
-						}
-					}
-				});
-			});
-		</script>
 		<div id="accordion">
 			<?php while (have_rows('accordion_repeater')): the_row()?>
 				<h3><?php the_sub_field('accordion_title'); ?></h3>
@@ -75,3 +43,36 @@
 <?php endif; ?>
 
 <?php edit_post_link(); ?>
+
+<script>
+	$(function(){
+		// open and go to correct accordion-header when adding /#link-1 in url
+		var hashLink = window.location.hash;
+		if (hashLink){
+			// var hashLinkId = hashLink.split('-')[1];
+			setTimeout(function(){
+				$(hashLink).click();
+			}, 500);
+		}
+		$counter = 1;
+		$("#accordion h3").each(function(){
+			$(this).attr("id", "link-" + $counter);
+			$counter = $counter +1;
+		});
+		var icons = {
+			header: "iconClosed",    // custom icon class
+			activeHeader: "iconOpen" // custom icon class
+		};
+		$("#accordion").accordion({
+			icons: icons,
+			heightStyle: "content",
+			collapsible: true,
+			active: false,
+			activate: function( event, ui ){
+				if(!$.isEmptyObject(ui.newHeader.offset())) {
+					$('html:not(:animated), body:not(:animated)').animate({ scrollTop: ui.newHeader.offset().top - 30 }, 'slow');
+				}
+			}
+		});
+	});
+</script>
