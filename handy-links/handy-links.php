0c87d6872c7387e442f8a4fb490518faceb699c2
Verhoevenwout <Wout@Wout-Verhoeven.local>
2017-07-04 14:04:33 +0100

    update

---
+++ handy-links/handy-links.php
@@ -0,0 +1,24 @@
+<!-- Compress Pdf -->
+  https://smallpdf.com/
+<!-- Check Internet Speed -->
+  https://fast.com/
+
+<!-- Wordpress Dashboard Icons -->
+  https://developer.wordpress.org/resource/dashicons/
+<!-- Create Placeholder Images -->
+  https://placeholder.com/
+<!-- Free Stock Images -->
+  https://unsplash.com/
+  https://www.pexels.com/
+<!-- Free Stock Videos -->
+  https://coverr.co/
+  https://pixabay.com/videos/
+
+<!-- Css Gradient Generator -->
+  http://www.cssmatic.com/gradient-generator
+<!-- Hamburger Animations -->
+  https://jonsuh.com/hamburgers/
+
+<!-- Background Patterns -->
+https://www.toptal.com/designers/subtlepatterns/
+https://www.transparenttextures.com/
