421580f6648cb55339f6e54b3650ba9151eec4b3
Verhoevenwout <hello@verhoevenwout.be>
2018-01-30 21:40:39 +0100

    UPDATE

--- 1-code-snippets/1-php-snippets/acf/acf-flexible-content.php
+++ 1-code-snippets/1-php-snippets/acf/acf-flexible-content.php
@@ -25,3 +25,47 @@ while(has_sub_field("content")): ?>
 		</div>
 	<?php endif; ?>
 <?php endwhile; ?>
+
+
+<!-- OR -->
+
+
+<?php if( have_rows('flex_text') ): ?>
+<?php while ( have_rows('flex_text') ) : the_row(); ?>
+
+	<?php if( get_row_layout() == 'flex_text_full' ): ?>
+		<div class="row section">
+			<div class="columns small-12">
+				<p>
+					<?php the_sub_field('flex_text_full_width') ?>
+				</p>
+			</div>
+		</div>
+	<?php elseif( get_row_layout() == 'flex_image_left' ):  ?>
+		<div class="row section">
+			<div class="columns small-12 medium-6 xmedium-4">
+				<?php $image = get_sub_field('flex_image_left_image')['url'] ?>
+				<img src="<?= $image ?>" alt="">
+			</div>
+			<div class="columns small-12 medium-6 xmedium-8">
+				<p>
+					<?php the_sub_field('flex_image_left_text') ?>
+				</p>
+			</div>
+		</div>
+	<?php elseif( get_row_layout() == 'flex_image_right' ):  ?>
+		<div class="row section">
+			<div class="columns small-12 medium-6 xmedium-8">
+				<p>
+					<?php the_sub_field('flex_image_right_text') ?>
+				</p>
+			</div>
+			<div class="columns small-12 medium-6 xmedium-4">
+				<?php $image = get_sub_field('flex_image_right_image')['url'] ?>
+				<img src="<?= $image ?>" alt="">
+			</div>
+		</div>
+	<?php endif; ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
