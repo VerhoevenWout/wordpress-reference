e149a3464b7573f01fe9e9b25707d2ae751484f0
Wout Verhoeven <hallo@verhoevenwout.be>
2018-03-19 09:13:22 +0100

    update

---
+++ 1-code-snippets/3-styles/font-awesome/cdn.php
@@ -0,0 +1,2 @@
+<link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
